ec959e8aae583c637c22f2e7fab80814dc20af8f
Hrishikesh <hrishikesh.pandey9955@gmail.com>
2019-09-16 15:46:05 +0530

    Updated Secret key for privacy purposes
    Change your secret key for using the project optimally

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4#catyb+p2x96pf3m__ixmne%&x^4=o5-1qs)=2zssv%sct!h*'
+SECRET_KEY = 'Change your secret key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
